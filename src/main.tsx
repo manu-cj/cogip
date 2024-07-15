f2b0bb5333170126af1285c149b0a6762a4cd642
manu-cj <carenjotje@gmail.com>
2024-07-15 14:40:50 +0200

    merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App.tsx'
-import Register from './component/Register.tsx'
-import Login from './component/Login.tsx'
-
 import {
   createBrowserRouter,
   RouterProvider,
