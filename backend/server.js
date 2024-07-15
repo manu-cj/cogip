ac28b6d0e9bd2c7d4013a7e33af4b576f4f72973
Masy Jordan <masyjordan@gmail.com>
2024-07-15 14:45:24 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
-
 import express from "express";
 import dbConnect from "./dbConnect/connect.js";
 import userRoutes from "./routes/userRoutes.js";
-import companiesRoutes from "./routes/companiesRoutes.js";
-
 
 dbConnect();
 
@@ -11,10 +8,8 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 
-// Add routes here when they're ready
-
+// Adding routes
 app.use("/api/users", userRoutes);
-app.use("/api/companies", companiesRoutes);
 
 app.listen(port, () => {
   console.log("Port successfully open at port", port);
