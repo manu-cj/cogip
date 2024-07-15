e8b5dc9db438fcc327cf0653452bb0a2249253d8
Masy Jordan <masyjordan@gmail.com>
2024-07-15 17:06:38 +0200

    update get/Companies

--- backend/controllers/companiesController.js
+++ backend/controllers/companiesController.js
@@ -8,64 +8,31 @@ const getCompanies = async (req, res) => {
     // console.log("t");
     try{
       const companies = await Companies.find();
-      console.log(companies);
-      res.status(200).res.json(companies);
+      return res.status(200).json({ companies });
 
     } catch(err){
-      res.status(500).json({ message: "SERVER ERROR" });
+      res.status(500).json({ message: `SERVER ERROR : ${err.message}` });
     }
 
   };
 
+  const postCompanies = async (req, res) => {
 
-  // const postCompanies = async (req, res) => {
-  //   console.log("test du post");
+    try{
 
-  //   let {name, contry, vat, createdAt, updatedOn} = req.body;
-  //   const maxLen = 25;
-  //   const maxLenDate = 12; 
-  //   const maxLenVat = 2;
+      const { name, contry, vat, createdAt, updatedOn } = req.body;
+      const companies = new Companies({name, contry, vat, createdAt})
+      await companies.save();
 
-  //   name = sanitize(name);
-  //   contry = sanitize(contry);
-  //   vat = sanitize(vat);
-  //   createdAt = sanitize(createdAt);
-  //   updatedOn = sanitize(updatedOn);
+      const existingCompanies = await Companies.find({ name });
+      if(existingCompanies){
+        return res.status(400).json({ message: "Company name alreay use" });
+      }
 
-  //   if (!name || !contry || !vat) {
-  //     return res.status(400).json({
-  //       message: "Invalid request, please make sure all parameters are sent.",
-  //     });
-
-  //   }
-
-  //   if( createdAt || updatedOn > Date()){ // Verify if the input date is < of the Date.now
-  //     return res.status(400).json({
-  //       message: "Invalid request, your date is invalid.",
-  //     });
-  //   }
-
-  //   if (
-  //     name.length > maxLen ||
-  //     contry.length > maxLen ||
-  //     vat.length > maxLenVat
-  //   ) {
-  //     return res.status(400).json({
-  //       message: "Invalid request, max input length : 50 chars.",
-  //     });
-  //   }
-
-  //   try{
-
-  //     const existingCompanies = await Companies.find({ name });
-  //     if(existingCompanies){
-  //       return res.status(400).json({ message: "Company name alreay use" });
-  //     }
-
-  //   } catch(err){
-  //     res.status(500).json({ message: `SERVER ERROR ${error.message}` });
-  //   }
-  // };
+    } catch(err){
+      res.status(500).json({ message: `SERVER ERROR ${error.message}` });
+    }
+  };
 
 
-export  { getCompanies };
+export  { getCompanies , postCompanies};
