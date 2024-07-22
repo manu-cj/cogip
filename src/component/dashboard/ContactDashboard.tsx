d149a7d165b9a46a1f9dd2a58356b5c3a73d648e
manu-cj <carenjotje@gmail.com>
2024-07-22 15:10:43 +0200

    add user data in the dashboard

--- src/component/dashboard/ContactDashboard.tsx
+++ src/component/dashboard/ContactDashboard.tsx
@@ -168,21 +168,24 @@ function ContactDashboard() {
         setCompanyClassName(e.target.value === 'default' ? 'default' : '');
       };
 
+      const getCookie = (name: string): string | null => {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) {
+          return parts.pop()?.split(';').shift() || null;
+        }
+        return null;
+      };
+
   return (
     <div className="dashBoard">
       <Hamburger
         className={`hamburger ${isOpen ? "" : "hidden"}`}
         toggle={handleClick}
       />
-      <NavBarLat
-        img="./../../../public/assets/img/unbgcommeunautre.jpg"
-        firstName="Dylan"
-        lastName="Feys"
-        className={`navBarLat ${isOpen ? "hidden" : "visible"}`}
-        toggle={handleClick}
-      />
+      <NavBarLat img={getCookie("imageName")} firstName={getCookie("firstName")}  lastName={getCookie("lastName")} className={`navBarLat ${isOpen ? 'hidden' : 'visible'}`} toggle={handleClick}/>
       <div className="dashBoard__content">
-        <Header firstName="Dylan" />
+        <Header firstName={getCookie("firstName")}/>
         <div className="dashBoard__contact">
           <h3>New contact</h3>
           <Notification notification={notification} />
