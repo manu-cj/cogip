0d26f74fefd585a97396a1596a8624b696495b24
manu-cj <carenjotje@gmail.com>
2024-07-25 13:14:05 +0200

    add vercel link for cors

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ const corsOptions = {
     const allowedOrigins = [
       'http://127.0.0.1:5173',
       'http://localhost:5173',
+      'https://cogip-h7w7n35kc-manu-cjs-projects.vercel.app/',
     ];
 
     if (allowedOrigins.includes(origin) || !origin) {
