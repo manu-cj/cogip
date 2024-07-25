847fc0d37bb537a0142675bcf5772da258028bcc
manu-cj <carenjotje@gmail.com>
2024-07-25 16:37:06 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import dbConnect from '../dbConnect/connect.js';
-import userRoutes from '../routes/userRoutes.js';
-import contactRoutes from '../routes/contactRoutes.js';
-import companiesRoutes from '../routes/companiesRoutes.js';
-import invoiceRoutes from '../routes/invoiceRoutes.js';
-import imageRoutes from '../routes/imageRoutes.js';
-import miscRoutes from '../routes/miscRoutes.js';
-import typeRoutes from '../routes/typeRoutes.js';
-import roleRoutes from '../routes/roleRoutes.js';
-import cookieRoutes from '../routes/cookieRoutes.js';
+import express from "express";
+import cors from "cors";
+import dbConnect from "./dbConnect/connect.js";
+import userRoutes from "./routes/userRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
+import companiesRoutes from "./routes/companiesRoutes.js";
+import invoiceRoutes from "./routes/invoiceRoutes.js";
+import imageRoutes from "./routes/imageRoutes.js";
+import miscRoutes from "./routes/miscRoutes.js";
+import typeRoutes from "./routes/typeRoutes.js";
+import roleRoutes from "./routes/roleRoutes.js";
+import cookieRoutes from "./routes/cookieRoutes.js";
 import cookieParser from 'cookie-parser';
-
 dbConnect();
 
 const app = express();
+const port = 3000;
+
+// const corsOptions = {
+//   origin: 'http://localhost:5173', // Remplacez par l'origine de votre frontend
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
+//   allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
+//   credentials: true // Pour autoriser les cookies
+// };
 
 // Middleware pour analyser le corps de la requête JSON
 app.use(express.json());
@@ -24,7 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware pour gérer les cookies
 app.use(cookieParser());
-
 const corsOptions = {
   origin: (origin, callback) => {
     const allowedOrigins = [
@@ -57,7 +63,10 @@ app.use("/api/types", typeRoutes);
 app.use("/api/roles", roleRoutes);
 app.use("/api/cookie", cookieRoutes);
 
-// Exposer l'application comme une fonction serverless pour Vercel
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
 export default (req, res) => {
   app(req, res);
 };
