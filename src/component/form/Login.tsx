a03ab410f2392d15a1efca52f24f0462bc078b4a
manu-cj <carenjotje@gmail.com>
2024-07-25 13:47:45 +0200

    update link login

--- src/component/form/Login.tsx
+++ src/component/form/Login.tsx
@@ -35,7 +35,7 @@ function Login() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();{
       try {
-        const response = await fetch('http://localhost:3000/api/users/login', {
+        const response = await fetch('https://cogip-61jf46xrk-manu-cjs-projects.vercel.app/api/users/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
