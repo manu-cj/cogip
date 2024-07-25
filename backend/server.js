c69d94e42a34069ec428ceca48469e6216f4695c
manu-cj <carenjotje@gmail.com>
2024-07-25 16:25:50 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -66,3 +66,7 @@ app.use("/api/cookie", cookieRoutes);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+export default (req, res) => {
+  app(req, res);
+};
