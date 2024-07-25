ad5ee2a93360902906586e80c54d2f37f29f8fa7
Caroline <deconinck.caro@gmail.com>
2024-07-25 09:43:24 +0200

    saving

--- backend/server.js
+++ backend/server.js
@@ -10,33 +10,24 @@ import miscRoutes from "./routes/miscRoutes.js";
 import typeRoutes from "./routes/typeRoutes.js";
 import roleRoutes from "./routes/roleRoutes.js";
 import cookieRoutes from "./routes/cookieRoutes.js";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 dbConnect();
 
 const app = express();
 const port = 3000;
 
-// const corsOptions = {
-//   origin: 'http://localhost:5173', // Remplacez par l'origine de votre frontend
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
-//   allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
-//   credentials: true // Pour autoriser les cookies
-// };
-
-// Middleware pour analyser le corps de la requête JSON
 app.use(express.json());
 
-// Middleware pour analyser les données de formulaire URL-encodées
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware pour gérer les cookies
 app.use(cookieParser());
 
-// Middleware CORS pour autoriser les requêtes cross-origin
-app.use(cors({
-  origin: 'http://127.0.0.1:5173', // Remplacez par l'URL de votre frontend
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/contacts", contactRoutes);
